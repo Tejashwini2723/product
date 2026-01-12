21262da42d9cdedcbcbf927278f0d7bca0a51d85
Tejashwini2723 <tejashwinihattarki@gmail.com>
2026-01-13 00:09:02 +0530

    Fix import paths in ProductGrid component

--- src/app/components/ProductGrid.tsx
+++ src/app/components/ProductGrid.tsx
@@ -1,4 +1,4 @@
-import { Product } from "../../types/product";
+import { Product } from '../types/product';
 import ProductCard from "../ProductCard";
 
 interface Props {
@@ -18,6 +18,7 @@ export default function ProductGrid({ products }: Props) {
     </div>
   );
 }
+
 
 
 
