de72444c4401253608f2a605f790c77466c1b380
Tejashwini2723 <tejashwinihattarki@gmail.com>
2026-01-12 23:52:29 +0530

    Refactor ProductCard component for better readability

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Product } from "../../types/product";
+import { Product } from "@/types/product";
 import { toggleFavorite, getFavorites } from "@/lib/favorites";
 import { useEffect, useState } from "react";
 import Link from "next/link";
@@ -85,3 +85,4 @@ export default function ProductCard({ product }: Props) {
   );
 
 }
+
